7e8868aaae3764d2f58a159fb4ed13c258502c10
johnzwiss <johnzwiss@gmail.com>
2022-02-09 18:42:13 -0500

    Add age

--- js/age.js
+++ js/age.js
@@ -1,1 +1,20 @@
+let age = 5
 
+if (age <2) {
+  console.log ("you are an infant!")
+}
+else if (age <4) {
+  console.log("you are a toddler")
+}
+else if (age <9) {
+  console.log("you are a child") 
+}
+else if (age <12){
+  console.log("you are a preteen")
+}
+else if (age <18){
+  console.log("you are a teen")
+}
+else if (age <21) {
+  console.log("you are a young adult")
+}
