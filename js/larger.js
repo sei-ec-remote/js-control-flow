8e29a49cf44e875aad4866ed56cf4b11dd99daf9
johnzwiss <johnzwiss@gmail.com>
2022-02-09 18:43:03 -0500

    Add larger

---
+++ js/larger.js
@@ -0,0 +1,9 @@
+let numA = 8
+let numB = 13
+
+if (numA > numB) {
+  console.log("The larger number of " + numA + " and " + numB + " is " + numA)
+} 
+else {
+  console.log("The larger number of " + numA + " and " + numB + " is " + numB)
+}
