4bd173a9b9de276db1c195c93714caa29e6fc133
johnzwiss <johnzwiss@gmail.com>
2022-02-09 18:44:09 -0500

    Add tempConvert

---
+++ js/tempConvert.js
@@ -0,0 +1,11 @@
+let temp = 55;
+
+let tempK = (temp - 32) * (5/9) + 273.15
+
+let tempC = (temp - 32) * (5/9)
+
+
+console.log(temp + " degrees farenheit is " + tempK +" degrees Kelvin")
+
+console.log(temp + " degrees farenheit is " + tempC +" degrees Celsius")
+
