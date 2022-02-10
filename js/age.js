8eed23cb83cc85b61b76a2f9af18289a53dc806e
dnierman0920 <davidnierman93@gmail.com>
2022-02-09 19:15:42 -0800

    add functionality/code to age.js

--- js/age.js
+++ js/age.js
@@ -1,1 +1,40 @@
+//NOTE THIS PROGAM WILL ONLY WORK IF THE AGE IS 1-20 (WHICH I AM ASSUMING BASED OFF THE INSTRCUTIONS)
+
+//create a variable to store the age of said person
+
+let age = 0
+
+
+//Your program should have a variable to store the age (an integer between 0 and 20)
+//for fun I am going to make the variable a list that uses the element location to represent age
+const ageGroups = ["infant", "infant","toddler","toddler","toddler", "child","child",
+ "child", "child", "child",  "preteen", "preteen", "teen", "teen",  "teen",  "teen", 
+  "teen", "young adult", "young adult", "young adult", "young adult"]
+
+//TESTING BELOW THIS LINE WITH VARIOUS AGES
+
+age = 0
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+
+age = 2
+
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+age = 7
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+ 
+age = 10
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+ 
+age = 13
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+
+age = 19
+
+console.log(`if you are ${age} then you are a ${ageGroups[age]}`)
+
 
