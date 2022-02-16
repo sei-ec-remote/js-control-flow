f1189aba1191e09f5627925c98e6695ed5984b85
katherine-kania <katherine.kania@gmail.com>
2022-02-16 18:24:49 -0500

    Revise comparingArrays complete

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -7,19 +7,26 @@ const arr4 = [0, 2, 4, 6];
 
 function compareArr(a, b){
 
-    // is length of array a unequal to length of array b
-    if (a.length != b.length) {
-        return false
-    } else {
-        // loop through each element and compare length
-        for (let i = 0; i < a.length; i++) {
-            if (arr1.length != b.length){
-                return false
-            } else {
-                return true
+     // is length of array a unequal to length of array b
+    if(a.length != b.length) {
+        return false;
+    } 
+    else { 
+        let result = false;
+        
+        // loop through to compare each index
+        for(let i = 0; i < a.length; i++) {
+
+            if(a[i] != b[i]) {
+                return false;
+            }
+            else {
+                result = true;
             }
         }
-    }
+        return result;     
+  }
+
 }
 
 console.log(compareArr(arr1, arr2))
