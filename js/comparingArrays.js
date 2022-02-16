842d64a09368ac184a42b6722a778b02c0fd5099
chrislee1996 <prepchrislee@gmail.com>
2022-02-16 12:46:44 -0500

    updated the comparingArrays problem! Logs once now instead of the multiple lines from before!

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -9,13 +9,13 @@ const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 const arr4 = [0, 2, 4, 6];
 
 
-if(arr1.length == arr2.length) {
-  console.log(true)
-} else {
-  console.log(false)
+
+let isEqual = true
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr3[i] ) {
+        isEqual = false
+    }
 }
-for (let i =0; i< arr1.length ;i++) {
-  if (arr1[i] === arr2[i]) {
-    console.log(true)
-  } else console.log(false)
-} 
+
+
+console.log(isEqual);
