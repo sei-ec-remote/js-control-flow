2eda95137b37710387967b032891ac94c4a0577e
“aquador” <Allison.quador@gmail.com>
2022-02-16 18:04:09 -0700

    Second commit after review

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -4,16 +4,14 @@ const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i']
 const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i']
 const arr4 = [0, 2, 4, 6]
 //use .values to break apart array
-const iterator = arr1.values()
 
-//declare an empty object
-
-const valueFrequencies = {}
-//loop over arr1 and print values
-for (const value of iterator) {
-  console.log(value)
-} if (valueFrequencies[arr1] === valueFrequencies[arr2]) {
-  console.log(true)
-} else if (valueFrequencies[arr1] === valueFrequencies[arr2]) {
-  console.log(false)
+if(arr1.length !== arr2.length) {
+  return false
 }
+ for (let i = 0; i < arr1.length; i++) {
+   if (arr1[i] !== arr2[i]) {
+     return false
+   } else {
+     console.log('equal')
+   }
+ }
