f0c20404a3f9a63153423ecc2f63e555b386ccbd
mitchgemma <mitchgemma@gmail.com>
2022-02-18 11:39:57 -0500

    resubmission Control-Flow

--- js/arrays.js
+++ js/arrays.js
@@ -1,14 +1,12 @@
 const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
-const arr2 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
+const arr2 = [1, 'B', 3, 5, 'e', 10, 'i'];
+const arr3 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
 let result = true
 
 
 for (let i = 0; i<arr1.length; i++)
 //conditional if all values and the length are the same
-// I was unsure how to make only 1 message for equal or not eqaual
-if (arr1[i] === arr2[i] && arr1.length === arr2.length) {
-    console.log("These arrays are equal.")
-// conditional if 1 condition is not met
-    } else if (arr1[i] !== arr2[i] || arr1.length !== arr2.length){
-      console.log("These arrays are not equal.")
+if (arr1[i] !== arr2[i]) {
+    result = false
     }
+console.log(result)
--- js/pluralizer.js
+++ js/pluralizer.js
@@ -2,9 +2,12 @@ let thing = 'burger'
 let count = 2
 
 // if count = 1, it is singular - therefore does not need an 's'
-if (count === 1){
-    console.log(count + " " + thing)
-    // everything else will be pluralized the same way
-} else {
-    console.log( count + " " + thing + "s")
+
+
+    if (count > 1){
+    console.log(count + " " + thing + "s")
+} else if (count === 1) {
+    console.log( count + " " + thing)
+}else {
+    console.log(count + " " + thing + "s")
 }
