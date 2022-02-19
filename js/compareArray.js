63aaf0eaedeeba3cb97d6a87aae274219e840762
jscodeguy <im.still.at.school99@gmail.com>
2022-02-19 14:36:28 -0500

    fixed my compare arrays

--- js/compareArray.js
+++ js/compareArray.js
@@ -2,18 +2,19 @@
 // let b = [1,2,3];
 // console.log(a[1] === b[1]);
 
-const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
-const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
-const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
-const arr4 = [0, 2, 4, 6];
+const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i']
+const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i']
+const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i']
+const arr4 = [0, 2, 4, 6]
 
-for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr4[i]){
-         console.log('the arrays are equal')
-    }
-
-    else if (arr1[i] !== arr4[i]) {
-        console.log('these arrays are not equal')
-    }
-
+const compareArrays = () => {
+    if(arr1.length !== arr3.length)
+        console.log("False")
+    else {
+        for(let i = 0; i < arr1.length; i++)
+        if(arr1[i]!== arr3[i])
+        return "False"
+        return "True"
+        }
 }
+console.log(compareArrays());
