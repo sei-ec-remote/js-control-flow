04f9792570c1412a0f4feece9f4c7c7faa5bae4a
CameronASC20 <camerongarnier73@gmail.com>
2022-03-04 13:39:30 -0500

    Homework done

---
+++ js/arrays.js
@@ -0,0 +1,8 @@
+const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
+const arr4 = [0, 2, 4, 6];
+
+if (arr1.forEach === arr2.forEach) {
+    console.log('true')
+}
--- js/translator.js
+++ js/translator.js
@@ -1,3 +1,11 @@
 let en = english
 let es = spanish
 let fr = french
+
+if (en) {
+    console.log('Hello World')
+} else if (es) {
+    console.log('Hola Mundo')
+} else if (fr) {
+    console.log('Bonjour le monde')
+}
