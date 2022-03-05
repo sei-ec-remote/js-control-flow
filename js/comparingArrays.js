19a90ec2bb9bc0e02a9cf46341598fc0662c0107
lyee197 <liamsyee@gmail.com>
2022-03-04 20:47:29 -0500

    Finished Comparing Arrays

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -3,4 +3,14 @@ const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
 const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 const arr4 = [0, 2, 4, 6];
 
-console.log(arr1 !== arr2)
+
+const compare = (a,b) => {
+    if (a.length === b.length){
+      for (let i = 0; i < arr1.length; i++) {
+        if (a[i] !== b[i]) {
+          return false
+        } 
+      } return true
+    } return false
+}
+console.log(compare(arr1,arr4))
