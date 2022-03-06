c7eb21be81859a296f0bd5c5d0f1fdcce6e93281
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-03-06 14:18:57 -0500

    updated comparingArrays

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -10,27 +10,28 @@ to make sure your script fully works:
 
 //two arrays to be compared. I didn't test different combinations
 // because I couldn't figure out how to test for matching index
-const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr1 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 //establishing a merged array to store elements in new variable
 let mergedArray = []
 //establish for loop for arr1
-for (let i = 0; i < arr1.length; i++) {
-    //nest for loop for arr3
-    for (let j = 0; j < arr3.length; j++){
-        if (arr1[i] === arr3[j]){
-            //arr3 element loops for each arr1 element and this condition
-            //checks whether any number in arr3 matches with that of arr1
-            //if it does, it gets stored in mergedArray array
-            mergedArray = arr1[i]
-            //prints the elements in new array that have matching equality
-            //not by index, but by 
-            console.log(mergedArray)  
-           
-        }
-        
+const checkEquality = () => {
+    if (arr1.length !== arr3.length) {
+        return false
     }
+    else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr3[i]){
+                return false
+            }
+            else {
+                continue
+            }
+        }
+   
     
+    }
+    return true
 }
 
-
+console.log(checkEquality())
