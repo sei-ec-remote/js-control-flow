895add3a85e56a445d8191677d65a7ec6861ad8c
sanchadr002 <sanchadr002@gmail.com>
2022-04-06 15:52:02 -0700

    js-control-flow deliverable

--- js/age.js
+++ js/age.js
@@ -1,1 +1,25 @@
+//### age.js
+//Write a program that will print the age category, given a variable with an age. Display either "infant", "toddler", "child", "preteen", "teen", or "young adult", for an age that is an integer between 0 and 20.
 
+//**Requirements**
+//* Your program should have a variable to store the age (an integer between 0 and 20)
+//* For the age categories, you may use whatever aging scale you'd like
+
+
+//answer
+
+for (age = 0; age <= 20; age++){
+    if (age <= 1){
+        console.log("You're an infant!")
+    } else if (age <= 4){
+        console.log("You're a toddler!")
+    } else if (age <= 9){
+        console.log("You're a child!")
+    } else if (age <= 12){
+        console.log("You're a preteen!")
+    } else if (age <= 18){
+        console.log("You're a teen!")
+    } else if (age <= 20){
+        console.log("You're a young adult!")
+    }
+}
--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -22,8 +22,8 @@ let arr1 = [1, 2, 3, 4]
 let arr2 = [1, 2, 3, 4]
 
 for (let i; i < arr1.length; i++){
-    for (let y; y < arr2.length; y++)
-    if (arr1[i] === arr2[y]){
+    if (arr1[i] === arr2[i]){
         console.log('These elements are equal!')
     }
 }
+
