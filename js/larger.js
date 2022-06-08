55ace4ea3055673326c81a68ac8fb8a95155ec2b
Alys <alice.g.cooper@gmail.com>
2022-06-08 14:02:08 -0400

    Create Larger Number Guessing Program

---
+++ js/larger.js
@@ -0,0 +1,14 @@
+// Write a program that reads two Number variables and prints which one is greater. 
+// (e.g. "The larger number of 7 and 3 is 7")
+// Requirements
+// Your program should maintain two variables
+// It should correctly identify which is larger and print that result
+
+let variableOne = 4005;
+let variableTwo = 839;
+
+if (variableOne > variableTwo) {
+    console.log(`The larger number of ${variableOne} and ${variableTwo} is ${variableOne}.`);
+} else {
+    console.log(`The larger number of ${variableOne} and ${variableTwo} is ${variableTwo}.`);
+}
