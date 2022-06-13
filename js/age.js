2bd7fb4b183d20d52f86989ecaf4bed37710682b
soungbin <soungbin941230@gmail.com>
2022-06-13 05:38:04 -0700

    fixed the comment for age.js

--- js/age.js
+++ js/age.js
@@ -1,6 +1,26 @@
 
 let age = 15
 
-if ((age > 0) && ( age < 20)){
-    console.log("teen")
+if ((age > 0) && ( age < 3)){
+    console.log("infant")
 }
+else if ((age >= 3) && ( age < 6)){
+    console.log("toddler")
+}
+else if ((age >= 6) && ( age < 9)){
+    console.log("child")
+}
+else if ((age >= 9) && ( age < 11)){
+    console.log("preteen")
+}
+else if ((age >= 11) && ( age < 16) ) {
+    console.log("tenn")
+}
+else if( (age >= 16) && ( age <= 20)){
+    console.log("young adult")
+}
+else{
+    console.log("adult")
+}
+
+
