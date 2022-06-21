66fa801aa4196b7f2f2ec560b14006ee5938a0eb
soungbin <soungbin941230@gmail.com>
2022-06-21 01:14:05 -0700

    Completed Resubmit it

--- js/age.js
+++ js/age.js
@@ -14,7 +14,7 @@ else if ((age >= 9) && ( age < 11)){
     console.log("preteen")
 }
 else if ((age >= 11) && ( age < 16) ) {
-    console.log("tenn")
+    console.log("teen")
 }
 else if( (age >= 16) && ( age <= 20)){
     console.log("young adult")
@@ -23,4 +23,27 @@ else{
     console.log("adult")
 }
 
-
+ age = 30
+switch(true){
+    case age >= 16:
+        console.log("young adults");
+        break;
+    case age >= 11:
+        console.log("teen")
+        break;
+        break;
+    case age >= 9:
+        console.log("preteen")
+        break;
+    case age >= 6:
+        console.log("child")
+        break;
+    case age >=3:
+        console.log("toddler")
+        break
+    case age >=0:
+        console.log("infant")
+        break;
+    default:
+        console.log("error")
+}
