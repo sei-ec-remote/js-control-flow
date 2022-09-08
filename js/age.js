531251fcbea191e58b5cd017fd943c695859b900
nabbott98 <nicholasabbott98@gmail.com>
2022-09-08 17:45:58 -0400

    Program that will print the age category, given a variable with an age.

--- js/age.js
+++ js/age.js
@@ -1,1 +1,19 @@
+// Age Variable
+const age = 0;
 
+// If statements to determine age range
+if(age < 0){
+    console.log('That is not a human, or you are a time traveler')
+} else if(age < 2) {
+    console.log('infant')
+} else if(age < 4){
+    console.log('toddler')
+} else if(age < 10){
+    console.log('child')
+} else if(age < 13){
+    console.log('preteen')
+} else if(age < 19){
+    console.log('teen')
+} else {
+    console.log('young adult')
+}
