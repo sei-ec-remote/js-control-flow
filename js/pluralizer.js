1a08c5193c79f27276cce188cbc42aa47ba3b022
skbarbee <barbee.s.k@gmail.com>
2022-09-08 17:59:17 -0400

    adding pluralizer file

---
+++ js/pluralizer.js
@@ -0,0 +1,13 @@
+let thing = 'cat';
+let count = 5;
+console.log(thing)
+console.log(count)
+
+for (let count = 0; count < 6; count++){
+    if(count >1){
+        console.log(count + " " + thing + "s")
+    }else{
+        console.log(count + " " + thing)
+    }
+}
+
