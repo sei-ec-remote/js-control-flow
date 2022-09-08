35a5700b35422deaf365e01b8e686f1fc40838c4
mrbaummath <baum1286@gmail.com>
2022-09-08 17:47:10 -0400

    finished translator

---
+++ js/translator.js
@@ -0,0 +1,9 @@
+let newLang = "de"
+
+if (newLang === "es") {
+    console.log("Hola Mundo")
+} else if (newLang === "fr") {
+    console.log("Bonjour le Monde")
+} else if (newLang==='de') {
+    console.log ("Hallo Welt")
+}
