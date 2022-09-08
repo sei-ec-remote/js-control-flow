6ba9b63103cc839a301d60f36b2e1ad024b13d54
nabbott98 <nicholasabbott98@gmail.com>
2022-09-08 18:38:01 -0400

    Program that reads a variable with language code and prints the translation of Hello World for that language.

---
+++ js/translator.js
@@ -0,0 +1,16 @@
+// Initial language code variable
+// Translation options: en, es, fr, gr, emoji
+let lang = 'emoji'
+
+// Translate word
+if(lang === 'en'){
+    console.log('Hello World')
+} else if(lang === 'es'){
+    console.log('Hola Mundo')
+} else if(lang === 'fr'){
+    console.log('Bonjour Le Monde')
+} else if(lang === 'gr'){
+    console.log('Γειά σου Κόσμε')
+} else if(lang === 'emoji'){
+    console.log('👋 🌎')
+}
