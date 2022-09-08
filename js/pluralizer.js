1f588146b2d9c45b1035933abd4e4b2ff487d666
nabbott98 <nicholasabbott98@gmail.com>
2022-09-08 18:14:54 -0400

    Added comments for improved readabilty

--- js/pluralizer.js
+++ js/pluralizer.js
@@ -1,6 +1,8 @@
+// Declare initial variables
 let thing = 'cat';
 let count = 0;
 
+// Determine if the 'thing' should be pluralized then print out pluralized version
 if(count === 1){
     console.log(count + ' ' + thing)
 } else {
