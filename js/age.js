9437c53ed0419e553531e44c6e0c6caf8a9bd24a
melanie silva <melsil11@hotmail.com>
2022-09-08 18:14:25 -0400

    homework

--- js/age.js
+++ js/age.js
@@ -1,1 +1,14 @@
-
+let age = 0
+for (let age= 0; age < 21; age++) {
+    if (age <2){
+        console.log("Infant, it's baby") 
+    }else if(age < 5){
+        console.log("Toddler, let the mess begin!") 
+    }else if(age < 10){
+        console.log("Preteen, get ready to rumble")
+    }else if (age < 15){
+        console.log("Teen, it will get better")
+    }else if (age < 20){
+        console.log("Young adult, you made it!")
+    }
+} 
