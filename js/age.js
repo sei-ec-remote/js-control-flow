80fde41ceee531f73d00c0a37628d6d002a6b39a
OnePartHarmony <bakingharmony@gmail.com>
2022-09-08 18:38:10 -0400

    finished age.js

--- js/age.js
+++ js/age.js
@@ -1,1 +1,15 @@
+let age = 10
 
+if (age < 1){
+    console.log(`This ${age}-year-old person is an infant.`)
+} else if (age >= 1 && age < 3) {
+    console.log(`This ${age}-year-old person is a toddler.`)
+} else if (age >= 3 && age < 10) {
+    console.log(`This ${age}-year-old person is a child.`)
+} else if (age >= 10 && age < 13) {
+    console.log(`This ${age}-year-old person is a preteen.`)
+} else if (age >= 13 && age < 20) {
+    console.log(`This ${age}-year-old person is a teen.`)
+} else if (age = 20) {
+    console.log(`This ${age}-year-old person is a young adult.`)
+}
