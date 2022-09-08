c7b6b95e589d53f618430e331a0e473e3232a89a
skbarbee <barbee.s.k@gmail.com>
2022-09-08 18:32:03 -0400

    adding larger file

---
+++ js/larger.js
@@ -0,0 +1,11 @@
+//Write a program that reads two Number variables and prints which one is greater.
+// (e.g. "The larger number of 7 and 3 is 7")
+
+let number1 =3
+let number2 =4
+
+if(number1 > number2){
+    console.log("The larger number of " + number1 + " and " + number2 + " is " + number1)
+}else if(number2 > number1){
+    console.log("The larger number of " + number1 + " and " + number2 + " is " + number2)
+}
