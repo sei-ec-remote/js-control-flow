d7a4dcdbc7c38a6caa7a846591251c6f2a0ca66c
melanie silva <melsil11@hotmail.com>
2022-09-08 19:20:20 -0400

    initial homework

---
+++ js/larger.js
@@ -0,0 +1,7 @@
+let a= 7
+let b= 3
+if(a < b ){
+    console.log(" the larger number of " + a + " and " + b +  " is " + b)
+} else if (a > b){
+    console.log("the larger number of " + a + " and " + b + " is " + a)
+}
