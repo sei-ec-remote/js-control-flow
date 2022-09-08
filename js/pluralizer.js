0116cd139f76db631fd5e61df6445b491962e94b
mrbaummath <baum1286@gmail.com>
2022-09-08 17:51:58 -0400

    there were multiple instances of age and pluralizer so I just made sure my code was in both

---
+++ js/pluralizer.js
@@ -0,0 +1,5 @@
+//declare variable 'thing' for a noun to pluralize and a 'count' of how many 'things' there are
+let thing = 'cat'
+let count = 5
+
+console.log(count === 1 ? `${count} ${thing}` : `${count} ${thing}s`)
