56c4774b8a1d30d1803c9ee4602bd421122091db
OnePartHarmony <bakingharmony@gmail.com>
2022-09-08 20:04:58 -0400

    completed translator exercise

---
+++ js/translator.js
@@ -0,0 +1,13 @@
+let language = "fr"
+
+if (language == "en") {
+    console.log("Hello World")
+} else if (language == "es") {
+    console.log("Hola Mundo")
+} else if (language == "fr") {
+    console.log("Bonjour le Monde")
+} else if (language == "it") {
+    console.log("Ciao Mondo")
+} else {
+    console.log("The language you have chosen is not yet supported.")
+}
