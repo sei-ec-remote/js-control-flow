2f64658574b1ccbb07a1288c936e723a70910e8f
melanie silva <melsil11@hotmail.com>
2022-09-08 20:09:42 -0400

    initial homework

---
+++ js/translator.js
@@ -0,0 +1,11 @@
+let languageCode = "de"
+
+if(languageCode === "es"){
+    console.log("Hola Mundo!")
+}else if (languageCode === "de"){
+    console.log("Hallo Welt!")
+}else if (languageCode === "fr"){
+        console.log("Bonjour le monde!")
+        
+ }
+
