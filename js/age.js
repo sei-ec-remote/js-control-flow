5275c7e9a003a04eac6bdb39a2b209326eaae026
keyboard keys <keyboard@keyboards-iMac.local>
2022-09-08 21:02:57 -0400

    age.js deliv

--- js/age.js
+++ js/age.js
@@ -1,1 +1,26 @@
+for (let i = 0; i <= 20; i++) {
+    
+    if (i < 2){
+    
+ console.log('infant')
+}
+    else if (i < 5){
+        console.log('toddler')
+    }
+    else if (i < 12 ) {
+        console.log('child')
+    }
+    
+    else if (i < 15 ) {
+        console.log('preteen')
+    }
 
+    else if (i > 16) {
+        console.log('teen')
+    }
+    
+    else if (i < 20){
+        console.log('young adult')
+    }
+   
+}
