9da4ffcf3fd60ca273d7342fd19c7e2b7856820c
keyboard keys <keyboard@keyboards-iMac.local>
2022-09-08 21:00:42 -0400

    temp deliv

---
+++ js/tempConvert.js
@@ -0,0 +1,26 @@
+function cToF(celsius) {
+    var fahrenheit;
+    fahrenheit = celsius * 9/5 + 32;
+    return fahrenheit;
+}
+
+console.log("5 degrees Fahrenheit is " + cToF(5) + " degrees " + "Celsius");
+
+// "X degrees Fahrenheit is Y degrees Celsius/Kelvin"
+
+// fahrenheit = F;
+
+// while (fahrenheit <= 50) {
+//     celsius = (fahrenheit - 32) / 9 * 5;
+
+//     print "<tr><td>$fahrenheit</td><td>$celsius</td></tr>";
+
+//     fahrenheit += 5;
+// } 
+
+// let fahrenheit = parseFloat(prompt("Enter the temperature in fahrenheit: "))
+ 
+// let celsius = (fahrenheit - 32) / 1.8;
+ 
+// console.log(fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius");
+
