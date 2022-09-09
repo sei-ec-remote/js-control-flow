4700619fe05e2b1f58bfe75e73a078e77ae001e9
keyboard keys <keyboard@keyboards-iMac.local>
2022-09-08 22:33:54 -0400

    deliv 3

---
+++ js/translator.js
@@ -0,0 +1,15 @@
+// en = "Hello World"
+// amh = "Selam"
+// es = "Hola Mundo"
+
+// if (en)
+
+// console.log(en)
+
+let en = "Hello World"
+let amh = "Selam"
+let es = "Hola Mundo"
+
+console.log(amh)
+
+
