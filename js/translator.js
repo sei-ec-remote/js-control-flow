5a395c2e8d14511d2d63bae168eda1b1f13f20ba
isobelmorales <isofmorales@gmail.com>
2022-12-14 14:00:20 -0500

    Finished translator

---
+++ js/translator.js
@@ -0,0 +1,11 @@
+let languageCode = 'es';
+
+if(languageCode === 'es') {
+  console.log('Hola Mundo');
+} else if(languageCode === 'fr') {
+  console.log('Bonjour le Monde');
+} else if(languageCode === 'du') {
+  console.log('Hallo Wereld');
+} else {
+  console.log('Language is not supported. Please input es, fr, or du.');
+}
