758b588d449f257f92dbf52344cf5ffd3418bc98
rectorjordan94 <rectorjordan94@gmail.com>
2022-12-14 11:05:03 -0600

    edited age.js

--- js/age.js
+++ js/age.js
@@ -1,1 +1,19 @@
+let age;
 
+function ageCategory(age) {
+  if (age <= 3) {
+    console.log("infant");
+  } else if (age <= 6) {
+    console.log("toddler")
+  } else if (age <= 9) {
+    console.log("child")
+  } else if (age <= 12) {
+    console.log("preteen")
+  } else if (age <= 19) {
+    console.log("teen")
+  } else {
+    console.log("young adult");
+  }
+
+  ageCategory(10);
+}
