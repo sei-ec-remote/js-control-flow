4252d9c5fa6d8be5aa6e6d929f28d35de1426e9c
estebbins <stebbins.e@gmail.com>
2022-12-14 16:17:14 -0500

    Created and finalized tempConvert

---
+++ js/tempConvert.js
@@ -0,0 +1,27 @@
+//Write a program that converts a temperature from Fahrenheit to another temperature unit indicated by a variable.
+
+//Obtained the unit conversions from : https://nineplanets.org/kids/temperature-conversion/#:~:text=The%20basic%20formula%20is%20%C2%B0,%2F9%20%2B%20273.15%20%3D%20K. 
+
+//Obtained method to limit the number of decimals (toFixed())from https://www.geeksforgeeks.org/how-to-parse-float-with-two-decimal-places-in-javascript/ 
+
+let fahrenheit = 451
+let newUnit = 'k'
+let celsius = ((fahrenheit-32)*(5/9)).toFixed(2)
+let kelvin = (((fahrenheit-32)*(5/9))+273.15).toFixed(2)
+
+function tempConvert(fahrenheit, newUnit){
+  if(typeof fahrenheit === 'number' && (newUnit.toLowerCase() === 'c' || newUnit.toLowerCase() === 'k')){
+    if(kelvin < 0){
+      console.log('Sorry you have entered a number lower than absolute zero, which is.....absolute..ly...not....a....thing.....try again')
+    } else if(newUnit.toLowerCase() === 'c'){
+      console.log(`${fahrenheit} degrees Farenheit is ${celsius} degrees Celcius`)
+    } else if(newUnit.toLowerCase() === 'k'){
+      console.log(`${fahrenheit} degrees Farenheit is ${kelvin} degrees Kelvin`)
+      } 
+    }
+  else{
+    console.log("Please only enter a number for Fahrenheit and 'c' for Celsius or 'k' for Kelvin for a new unit.")
+  }
+  }
+
+tempConvert(fahrenheit, newUnit)
