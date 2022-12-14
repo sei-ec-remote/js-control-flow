30ab9add235b7e6ff3b1e89afd110c7eacd85aa7
isobelmorales <isofmorales@gmail.com>
2022-12-14 12:00:54 -0500

    Completed age.js program

--- js/age.js
+++ js/age.js
@@ -1,1 +1,20 @@
 
+let age = null
+// To test code
+// age = Math.floor(Math.random() * 20)
+
+if((age > 0) && (age < 2)) {
+    console.log('Age: ' + age + ' Category: Infant');
+} else if ((age >= 2) && (age <= 4)) {
+    console.log('Age: ' + age + ' Category: Toddler');
+} else if ((age >= 5) && (age <= 9)) {
+    console.log('Age: ' + age + ' Category: Child');
+} else if ((age >= 10) && (age <= 12)) {
+    console.log('Age: ' + age + ' Category: Preteen');
+} else if ((age >= 13) && (age <= 17)) {
+    console.log('Age: ' + age + ' Category: Teen');
+} else if ((age >= 18) && (age < 20)) {
+    console.log('Age: ' + age + ' Category: Young Adult');
+} else {
+  console.log('Input is invalid. Please enter an age between 0 and 20.');
+}
