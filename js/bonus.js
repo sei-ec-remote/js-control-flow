b9c207fc5ee803bd6f85eacc223e12f91113ef66
isobelmorales <isofmorales@gmail.com>
2022-12-14 16:29:14 -0500

    Completed bonus activity to rewrite age.js program using a switch statement. I created bonus.js to hold my answer.

---
+++ js/bonus.js
@@ -0,0 +1,65 @@
+// Setting age variable
+const age = 13;
+
+// Switch statement to print category based on age
+switch (age) {
+    case 1:
+        console.log('Age: 1 Category: Infant');
+        break;
+    case 2:
+        console.log('Age: 2 Category: Infant');
+        break;
+    case 3:
+        console.log('Age: 3 Category: Toddler');
+        break;
+    case 4:
+        console.log('Age: 4 Category: Toddler');
+        break;
+    case 5:
+        console.log('Age: 5 Category: Toddler');
+        break;
+    case 6:
+        console.log('Age: 6 Category: Child');
+        break;
+    case 7:
+        console.log('Age: 7 Category: Child');
+        break;
+    case 8:
+        console.log('Age: 8 Category: Child');
+        break;
+    case 9:
+        console.log('Age: 9 Category: Child');
+        break;
+    case 10:
+        console.log('Age: 10 Category: Child');
+        break;
+    case 11:
+        console.log('Age: 11 Category: Preteen');
+        break;
+    case 12:
+        console.log('Age: 12 Category: Preteen');
+        break;
+    case 13:
+        console.log('Age: 13 Category: Teen');
+        break;
+    case 14:
+        console.log('Age: 14 Category: Teen');
+        break;
+    case 15:
+        console.log('Age: 15 Category: Teen');
+        break;
+    case 16:
+        console.log('Age: 16 Category: Teen');
+        break;
+    case 17:
+        console.log('Age: 17 Category: Teen');
+        break;
+    case 18:
+        console.log('Age: 18 Category: Young Adult');
+        break;
+    case 19:
+        console.log('Age: 19 Category: Young Adult');
+        break;
+    default:
+        console.log('Input is invalid. Please enter an age between 0 and 20.');
+}
