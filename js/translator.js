c850fa19c0aeabdda7eccf1b03c5c4761732c4e8
estebbins <stebbins.e@gmail.com>
2022-12-14 17:30:40 -0500

    added translator.js

---
+++ js/translator.js
@@ -0,0 +1,28 @@
+// Write a program that reads a variable with language code and prints the translation of "Hello World" for that language.
+
+// Requirements
+
+// You should have a variable for the language code (e.g. "es", "de", "fr", "en").
+// Your program should print the translation of "Hello World" based on the language code in the variable.
+// It should support at least 3 languages
+// Make sure to test with the different languages you choose.
+// (Feel free to Google the translation if you aren't fluent.)
+
+// Languages - english, danish, icelandic - source: google.translate
+
+let lang = 'is'
+
+function translator(lang){
+  if(lang.toLowerCase() === 'es' ){
+    console.log("Hello world!")
+  } else if(lang.toLowerCase() ==='de'){
+    console.log("Hej Verden!")
+  } else if(lang.toLowerCase() === 'is'){
+    console.log("Hallo heimur")
+  }
+  else{
+    console.log("Sorry, that language is not supported at this time, try again later!")
+  }
+}
+
+translator(lang)
