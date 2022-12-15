c2ad9c0d571951458404295e3da5f557f0aaf22f
Briana Wright <brianaleew@gmail.com>
2022-12-14 22:00:18 -0500

    Finished age conditional

--- js/age.js
+++ js/age.js
@@ -1,1 +1,17 @@
+let age = 21
+
+if(age < 2) {
+    console.log("Infant")
+} else if (age >= 2 && age <= 4){
+    console.log("Toddler")
+} else if (age > 4 && age < 10){
+    console.log("Child")
+} else if (age > 10 && age < 13){
+    console.log("Preteen")
+} else if (age > 13 && age <= 17){
+    console.log("Teenager")
+} else if (age >= 18  && age <= 20){
+    console.log("Young Adult")
+}
+
 
