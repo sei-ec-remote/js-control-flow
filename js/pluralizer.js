70d70c257abb842e1ac2f3b9d5098cac0beebe5d
caitreid <ctlnrd@gmail.com>
2022-12-14 21:40:39 -0500

    pluralizer.js

--- js/pluralizer.js
+++ js/pluralizer.js
@@ -1,11 +1,22 @@
-// ### pluralizer.js
+// pluralizer.js
 // Write a program that starts like...
+//      let thing = 'cat';
+//      let count = 5;
+// and outputs the pluralized form of the word, depending on what `count` is. For example, "5 cats" or "1 dog". 
+// Change `thing` to different values to test your code!
 
-// ```js
-// let thing = 'cat';
-// let count = 5;
-// ```
-// and outputs the pluralized form of the word, depending on what `count` is. For example, "5 cats" or "1 dog". Change `thing` to different values to test your code!
-
-// **Requirements**
+// Requirements
 // * Your program should pluralize the word based on an integer (`count`)
+
+
+let thing = 'cat';
+
+let count = Math.floor(Math.random() * 5);
+
+
+if (count < 2) {
+    console.log(`Singular ${thing}. All on his own.`)
+}
+if (count >= 2 ) {
+    console.log(`Many ${thing}s! ${count} of them.`)
+}
