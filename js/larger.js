651e8fee552b0b416cb930a5b40594ba31967567
Briana Wright <brianaleew@gmail.com>
2022-12-14 23:15:55 -0500

    Finished larger task

---
+++ js/larger.js
@@ -0,0 +1,11 @@
+let a = 5 
+let b = 17
+
+if ( a > b ) {
+    console.log(`"The larger number of ${a} and ${b} is ${a}`)
+} else if ( a < b ) {
+    console.log(`"The larger number of ${b} and ${a} is ${b}`)
+} else {
+    console.log(` Neither ${a} nor ${b} is greater`)
+    
+}
