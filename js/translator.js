5b460fa34b89f059f24a89acade57871ada2f7b9
Briana Wright <brianaleew@gmail.com>
2022-12-15 19:44:01 -0500

    Completed translator

--- js/translator.js
+++ js/translator.js
@@ -1,4 +1,15 @@
-let fr = french
+let languageChoice = "fr" 
+const fr = "Bonjour le Monde"
+const sp = "Hola Mundo"
+const it = "Ciao Mondo"
+
+if (languageChoice === "fr") {
+    console.log(`Hello World in French is ${fr}.`)
+} else if (languageChoice === "sp") {
+    console.log(`Hello World is Spanish for ${sp}`)
+} else if (languageChoice === "it") {
+    console.log(`Hello World is Italian for ${it}`)
+}
 
 
 
@@ -6,6 +17,6 @@ let fr = french
 
 
 
-Bonjour le Monde  french
-Hola Mundo 
-Ciao Mondo 
+
+
+
