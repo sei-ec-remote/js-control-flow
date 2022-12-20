ef41a69dbb6e12c0b65073fe329ea96ece4b1851
jrh <joshuaharris1103@gmail.com>
2022-12-20 15:01:28 -0500

    made edits to compareArray and tempConvert to complete deliverable

--- js/compareArray.js
+++ js/compareArray.js
@@ -5,10 +5,19 @@ const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
 const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 
 const arr4 = [0, 2, 4, 6];
-
-console.log(arr1 === arr2);
-console.log(arr1 === arr3);
-console.log(arr1 === arr4);
-console.log(arr2 === arr3);
-console.log(arr2 === arr4);
-console.log(arr3 === arr4);
+const compareArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) return false;
+    else {
+      // Comparing each element of your array
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  };
+  
+  console.log(compareArrays(arr1, arr2)); 
+  console.log(compareArrays(arr1, arr3)); 
+  console.log(compareArrays(arr2, arr4)); 
--- js/tempConvert.js
+++ js/tempConvert.js
@@ -1,3 +1,5 @@
 let x = 67
 let c = (x - 32) * .5556
+let K = (x - 32) * (5/9) + 273.15
 console.log(`${x} degrees Fahrenheit is ${c} degrees Celcius`)
+console.log(`${x} degrees Fahrenheit is ${K} Kelvins`)
