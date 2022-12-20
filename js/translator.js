bd7bbfac95170f398fef70357dd885484abf63c1
Tayana <tayana1498@gmail.com>
2022-12-20 09:07:07 -0700

    Homework Completed

--- js/translator.js
+++ js/translator.js
@@ -1,11 +1,20 @@
 let en = 'Hello World!';
 let fr = 'Bonjour Monde!';
 let es = 'Hola Mundo!';
-
+/*
 {
     console.log(fr)
 }
-
+*/
+if (Math.random(en) <0.4) {
+    console.log("English translation is " + en)
+}   
+else if (Math.random(fr) <0.6){
+    console.log("The French translation of " + en + " is " + fr)
+}
+else if (Math.random(es) <0.8){
+    console.log("The Spanish translation of " + en + " is " + es)
+}
 /*
 {
     console.log(en)
