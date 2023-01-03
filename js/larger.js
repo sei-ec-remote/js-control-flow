7c409b533de998db2f75bdfd705bea438923c3fd
Aaron Humphres <aaronhumphres@yahoo.com>
2023-01-03 10:24:10 -0700

    committing

---
+++ js/larger.js
@@ -0,0 +1,13 @@
+//### larger.js
+//Write a program that reads two Number variables and prints which one is greater. (e.g. "The larger number of 7 and 3 is 7")
+
+//**Requirements**
+//* Your program should maintain two variables
+//* It should correctly identify which is larger and print that result
+
+let x = 7;
+let y = 3;
+
+if(x > y) console.log("The larger number of " + x + " and " + y + " is " + x)
+
+else console.log("The larger number of " + x + " and " + y + " is " + y)
